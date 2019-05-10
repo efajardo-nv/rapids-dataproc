51a580256b8f8225b1ce40da498f2b3037544fde
Randy Gelhausen <rgelhau@gmail.com>
2019-05-10 14:19:34 -0400

    Using larger instance type, push dask-cuda-worker logging to background

--- init-actions/dask-network-setup.sh
+++ init-actions/dask-network-setup.sh
@@ -6,5 +6,5 @@ MASTER=$(/usr/share/google/get_metadata_value attributes/dataproc-master)
 if [[ "${ROLE}" == 'Master' ]]; then
   dask-scheduler &
 else
-  dask-cuda-worker ${MASTER}:8786 &
+  dask-cuda-worker --memory-limit 0 ${MASTER}:8786 2>&1 &> /var/log/worker.log
 fi
--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -8,6 +8,7 @@ gcloud beta dataproc clusters create $NAME \
 --master-accelerator type=nvidia-tesla-t4 \
 --worker-accelerator type=nvidia-tesla-t4,count=$GPUS_PER_WORKER \
 --num-workers $WORKERS \
+--worker-machine-type n1-standard-32 \
 --metadata "JUPYTER_PORT=8888,gpu-driver=NVIDIA-Linux-x86_64-410.104.run" \
 --bucket $DATAPROC_BUCKET \
 --initialization-actions gs://$DATAPROC_BUCKET/init-actions/install-gpu-driver.sh,gs://$DATAPROC_BUCKET/init-actions/conda-env-setup.sh,gs://dataproc-initialization-actions/jupyter/jupyter.sh,gs://$DATAPROC_BUCKET/init-actions/dask-network-setup.sh
